581da29a921d89f023387d85a95256cf648ff52c
RBK-11 <rbarahona20416@ufide.ac.cr>
2026-08-10 18:08:56 -0600

    Sección Docentes

---
+++ EduNexus/EduNexus/EduNexus.Abstracciones/ModelosParaUI/AsignacionesDocentes/AsignacionDocenteDto.cs
@@ -0,0 +1,10 @@
+﻿namespace EduNexus.Abstracciones.ModelosParaUI.AsignacionesDocentes
+{
+    public class AsignacionDocenteDto
+    {
+        public int id_asignacion_docente { get; set; }
+        public int id_docente { get; set; }
+        public int id_materia { get; set; }
+        public int id_seccion { get; set; }
+    }
+}
